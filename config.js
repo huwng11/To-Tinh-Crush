18a5e7a987ddb430d0f311e5bc299bcffd20b7b5
huwng11 <87932174+huwng11@users.noreply.github.com>
2021-07-25 22:51:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
+    introTitle: 'Viên à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao Viên cứ đi nhầm đường
+    Lạc vào tim Hưng lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    title: 'Phải chăng Viên đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng Viên đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích Hưng <33',
+    btnNo: 'Không, mơ à :3',
+    question:'Thích Hưng ở điểm nào <3',
+    btnReply: 'Gửi cho Hưng <3',
+    reply: 'Tại vì Hưng đẹp trai nhức nách <33333333',
+    mess: 'Biết mà 🥰.Hihi 😘😘',
+    messDesc: 'Một ngày tốt lành nha Mễ Diên.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/profile.php?id=100012471608663' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
